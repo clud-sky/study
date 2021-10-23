071ce54b072632bb4fb3cb2f016e698a102245f1
liuyangjun <lyj1988_130@126.com>
2021-10-23 16:05:18 +0800

    [m]test

--- day08/sync/main.go
+++ day08/sync/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 	"sync"
+	"time"
 )
 
 var (
@@ -12,19 +15,31 @@ var (
 )
 
 func add() {
-	for i := 0; i < 5000; i++ {
+	for i := 0; i < 5; i++ {
 		lock.Lock()
 		x = x + 1
+		fmt.Println(x)
 		lock.Unlock()
 	}
 	defer wg.Done()
 }
 
 func main() {
+	value := strconv.FormatFloat(9.285, 'f', 2, 64)
+	fmt.Println(value)
+	return
 	wg.Add(3)
 	go add()
 	go add()
 	go add()
 	wg.Wait()
+	time.Sleep(time.Second * 10)
 	fmt.Println(x)
+
+	sss := 3.1415
+	fmt.Println(math.Round(sss))
+}
+
+func round(x float64) float64 {
+	return float64(math.Floor(x + 0/5))
 }
